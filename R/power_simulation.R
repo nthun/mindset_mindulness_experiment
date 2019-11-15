6ec8c59468ee4db57829ae1c9a34554ef780d6ed
nthun <thomassocska@yahoo.com>
2019-11-15 20:35:43 +0100

    Separate data generation from data analysis

--- R/power_simulation.R
+++ R/power_simulation.R
@@ -11,7 +11,7 @@ plan(multisession(workers = availableCores())) # Setting parallel processing
 # Define parameters -----------------------------------------------------------------
 
 samples = 1000 # Number of samples for each n
-cell_sizes = 10 # The increment of cell sizes for the simulation
+cell_sizes = 30 # The increment of cell sizes for the simulation
 max_n = 200 # The maximum cell size
 lambda = 3 # The average number of extra tasks finished by the control group
 difference = .7 # The difference of the treatment group from the control
@@ -20,30 +20,62 @@ alpha = .05
 # Simulate samples ------------------------------------------------------------------
 # Generate a design matrix that defines the samples that will be simulated
 power_matrix <-
-  crossing(cell_size = seq.int(30, max_n, cell_sizes),
+  crossing(cell_size = seq.int(cell_sizes, max_n, cell_sizes),
            sample = 1:samples) 
 
 # Calculate the p values for each sample, based on the population parameters
-simulation_result <-
+simulation_data <-
   power_matrix %>% 
   as.list() %>% 
   # Use parallel processing to generate datasets(samples) based on parameters
-  # Then test the hypothesis on each dataset, and return the one-sided p-value
-  future_pmap_dbl(.progress = TRUE, 
-                  ~crossing(.x, 
-                            group = fct_inorder(c("Control", "Mindfulness")),
-                            id = 1:..1) %>% 
+  future_pmap(.progress = TRUE, 
+              ~crossing(group = fct_inorder(c("Fixed-Control", 
+                                              "Growth-Mindfulness")),
+                        id = 1:..1) %>% 
                 # Generate the number of extra tasks using truncated poisson distribution
-                # as the maximum number of extra tasks is 10.
+                # as the maximum number of extra tasks is 15.
                 mutate(extra_task = c(rtpois(n = ..1, lambda = lambda, b = 15), 
-                                      rtpois(n = ..1, lambda = lambda + difference, b = 15))) %>%
+                                      rtpois(n = ..1, lambda = lambda + difference, b = 15)))
+                
+  )
+
+
+# Test the hypothesis on each dataset
+simulation_results <-
+  simulation_data %>% 
+  future_map(.progress = TRUE, 
                 # Use poisson regression to test hypothesis
-                glm(extra_task ~ group, family = "poisson", data = .) %>% 
-                broom::tidy() %>% 
-                filter(term == "groupMindfulness") %>% 
-                # Calculate one sided Wald statistic
-                mutate(larger = pnorm(statistic, lower.tail = FALSE)) %>% 
-                pull(larger)) %>% 
+                ~glm(extra_task ~ group, family = "poisson", data = .x) %>% 
+                broom::tidy(exponentiate = TRUE))
+
+risk_ratios <-
+  simulation_results %>% 
+  map_dbl(.progress = TRUE, 
+                 ~filter(.x, term == "groupMindfulness") %>% 
+                  pull(estimate)) %>% 
+  # Put the RR values into the design matrix
+  bind_cols(power_matrix, rr = .)
+
+risk_ratios %>% 
+  group_by(cell_size) %>% 
+  summarise(rr_mean = mean(rr),
+            rr_se = sd(rr)/sqrt(n()))
+
+
+risk_ratios %>% 
+  ggplot() +
+  aes(x = cell_size, y = rr, group = cell_size) +
+  geom_boxplot() +
+  expand_limits()
+  
+
+p_values <- 
+  simulation_results %>% 
+  future_map_dbl(.progress = TRUE, 
+                  ~filter(.x, term == "groupMindfulness") %>% 
+                  # Calculate one-sided Wald statistic
+                  mutate(larger = pnorm(statistic, lower.tail = FALSE)) %>% 
+                  pull(larger)) %>% 
   # Put the p values into the design matrix
   bind_cols(power_matrix, p = .)
 
@@ -51,12 +83,11 @@ simulation_result <-
 # Calculate the power, based on the samples -----------------------------------------
 # Calculate the average p value and power for each cell size
 result_summary <-
-  simulation_result %>% 
+  p_values %>% 
   mutate(significant = p <= alpha) %>% 
   group_by(cell_size) %>% 
   summarise(power = mean(significant),
             p = mean(p))
-
 
 # Interpolate power values to see exact cut off point -------------------------------
 # Interpolate data, so we can tell exact N without calculating it to exact number
@@ -98,3 +129,33 @@ result_summary %>%
                      labels = scales::percent_format(), 
                      breaks = seq(.1, 1, .1))
 
+
+
+
+
+# Sandbox ---------------------------------------------------------------------------
+
+cell_sizes = 100
+
+# increase the number of groups
+
+df <- 
+  crossing(group = fct_inorder(c("Fixed-Control", 
+                                 "Fixed-Mindfulness",
+                                 "Growth-Control",
+                                 "Growth-Mindfulness")),
+         id = 1:cell_sizes) %>% 
+  mutate(extra_task = c(rtpois(n = cell_sizes, lambda = lambda, b = 15), 
+                        rtpois(n = cell_sizes, lambda = lambda + difference/2, b = 15), 
+                        rtpois(n = cell_sizes, lambda = lambda + difference/2, b = 15), 
+                        rtpois(n = cell_sizes, lambda = lambda + difference, b = 15))) %>% 
+  separate(group, c("mindset", "intervention"))
+  
+
+df %>% 
+  group_by(mindset, intervention) %>% 
+  summarise(mean(extra_task))
+
+glm(extra_task ~ intervention * mindset, family  = "poisson", data = df) %>% 
+  tidy()
+  
