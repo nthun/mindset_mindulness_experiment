6807a3249d575160f97b9320f2e5ab15dd120baf
nthun <thomassocska@yahoo.com>
2019-12-02 14:39:52 +0100

    Finalize power calculation

--- R/power_simulation.R
+++ R/power_simulation.R
@@ -10,17 +10,18 @@ plan(multisession(workers = availableCores())) # Setting parallel processing
 
 # Define parameters -----------------------------------------------------------------
 
-samples = 1000 # Number of samples for each n
-cell_sizes = 30 # The increment of cell sizes for the simulation
+samples = 10000 # Number of samples for each n
+sample_increment = 20 # The increment of cell sizes for the simulation
 max_n = 200 # The maximum cell size
-lambda = 3 # The average number of extra tasks finished by the control group
-difference = .7 # The difference of the treatment group from the control
-alpha = .05
+lambda = 4 # The average number of extra tasks finished by the control group
+max_lambda = 15 # Maximum number of extra tasks
+difference = 1.7 # The difference of the treatment group from the control
+alpha = .05 # Level of significance
 
 # Simulate samples ------------------------------------------------------------------
 # Generate a design matrix that defines the samples that will be simulated
 power_matrix <-
-  crossing(cell_size = seq.int(cell_sizes, max_n, cell_sizes),
+  crossing(cell_size = seq.int(sample_increment, max_n, sample_increment),
            sample = 1:samples) 
 
 # Calculate the p values for each sample, based on the population parameters
@@ -36,10 +37,10 @@ simulation_data <-
                         id = 1:..1) %>% 
                 # Generate the number of extra tasks using truncated poisson distribution
                 # as the maximum number of extra tasks is 15.
-                mutate(extra_task = c(rtpois(n = ..1, lambda = lambda, b = 15), 
-                                      rtpois(n = ..1, lambda = lambda + difference/2, b = 15), 
-                                      rtpois(n = ..1, lambda = lambda + difference/2, b = 15), 
-                                      rtpois(n = ..1, lambda = lambda + difference, b = 15))) %>% 
+                mutate(extra_task = c(rtpois(n = ..1, lambda = lambda, b = max_lambda), 
+                                      rtpois(n = ..1, lambda = lambda + difference/2, b = max_lambda), 
+                                      rtpois(n = ..1, lambda = lambda + difference/2, b = max_lambda), 
+                                      rtpois(n = ..1, lambda = lambda + difference, b = max_lambda))) %>% 
                 separate(group, into = c("mindset", "intervention"))
   )
 
@@ -49,67 +50,103 @@ simulation_results <-
   simulation_data %>% 
   future_map(.progress = TRUE, 
                 # Use poisson regression to test hypothesis
-                ~glm(extra_task ~ group, family = "poisson", data = .x) %>% 
+                ~glm(extra_task ~ intervention * mindset, family = "poisson", data = .x) %>% 
+               # Get Relative Risks and confidence intervals for the predictors
                 broom::tidy(exponentiate = TRUE, conf.int = TRUE))
 
-risk_ratios <-
+# Calculate relative risks
+rr_intervention <-
   simulation_results %>% 
-  map_dbl(.progress = TRUE, 
-                 ~filter(.x, term == "groupMindfulness") %>% 
-                  pull(estimate)) %>% 
+  map_dfr(.progress = TRUE, 
+                 ~filter(.x, term == "interventionMindfulness") %>% 
+                  select(estimate, conf.low, conf.high)) %>% 
   # Put the RR values into the design matrix
-  bind_cols(power_matrix, rr = .)
+  bind_cols(power_matrix, .)
 
-risk_ratios %>% 
+rr_intervention %>% 
   group_by(cell_size) %>% 
-  summarise(rr_mean = mean(rr),
-            rr_se = sd(rr)/sqrt(n()))
+  summarise(mean(estimate))
 
+rr_mindset <-
+  simulation_results %>% 
+  map_dfr(.progress = TRUE, 
+          ~filter(.x, term == "mindsetGrowth") %>% 
+            select(estimate, conf.low, conf.high)) %>% 
+  # Put the RR values into the design matrix
+  bind_cols(power_matrix, .)
 
-risk_ratios %>% 
-  ggplot() +
-  aes(x = cell_size, y = rr, group = cell_size) +
-  geom_boxplot() +
-  expand_limits()
-  
+rr_mindset %>% 
+  group_by(cell_size) %>% 
+  summarise(mean(estimate))
 
-p_values <- 
+# Calculate power
+p_intervention <- 
   simulation_results %>% 
   future_map_dbl(.progress = TRUE, 
-                  ~filter(.x, term == "groupMindfulness") %>% 
+                 ~filter(.x, term == "interventionMindfulness") %>% 
                   # Calculate one-sided Wald statistic
                   mutate(larger = pnorm(statistic, lower.tail = FALSE)) %>% 
                   pull(larger)) %>% 
   # Put the p values into the design matrix
   bind_cols(power_matrix, p = .)
 
+p_mindset <- 
+  simulation_results %>% 
+  future_map_dbl(.progress = TRUE, 
+                 ~filter(.x, term == "mindsetGrowth") %>% 
+                   # Calculate one-sided Wald statistic
+                   mutate(larger = pnorm(statistic, lower.tail = FALSE)) %>% 
+                   pull(larger)) %>% 
+  # Put the p values into the design matrix
+  bind_cols(power_matrix, p = .)
 
 # Calculate the power, based on the samples -----------------------------------------
-# Calculate the average p value and power for each cell size
+# Calculate the average p value and power for each cell size in cases where both or any group difference is significant
+
 result_summary <-
-  p_values %>% 
-  mutate(significant = p <= alpha) %>% 
+  left_join(p_intervention, p_mindset, by = c("cell_size", "sample")) %>% 
+  mutate(sig_int = p.x <= alpha,
+         sig_mind = p.y <= alpha,
+         sig_both = sig_int & sig_mind,
+         sig_any = sig_int | sig_mind) %>% 
   group_by(cell_size) %>% 
-  summarise(power = mean(significant),
-            p = mean(p))
+  summarise(power_both = mean(sig_both),
+            p_both = mean(p.x),
+            power_any = mean(sig_any),
+            p_any = mean(p.y))
 
 # Interpolate power values to see exact cut off point -------------------------------
 # Interpolate data, so we can tell exact N without calculating it to exact number
 interpolated_power <-
   tibble(cell_size = 1:max_n) %>% 
   left_join(result_summary, by = "cell_size") %>% 
-  # Use cubic spline interpolation
-  mutate(spline_power = stats::spline(
-                        x = cell_size,
-                        y = power,
-                        xout = cell_size)$y)
+  # Use linear interpolation
+  mutate(power_both = stats::approx( x = cell_size,
+                                     y = power_both,
+                                     xout = cell_size)$y,
+         p_both = stats::approx(x = cell_size,
+                                y = p_both,
+                                xout = cell_size)$y,
+         power_any = stats::approx( x = cell_size,
+                                    y = power_any,
+                                    xout = cell_size)$y,
+         p_any = stats::approx(x = cell_size,
+                               y = p_any,
+                               xout = cell_size)$y
+         ) %>% 
+  drop_na()
 
 # Calculate the exact number of participants needed in each cell for specific power
 interpolated_power %>% 
   transmute(cell_size,
-            `.80` = spline_power > .80,
-            `.90` = spline_power > .90,
-            `.95` = spline_power > .95) %>% 
+            `both = .80` = power_both > .80,
+            `both = .90` = power_both > .90,
+            `both = .95` = power_both > .95,
+            `both = .99` = power_both > .99,
+            `any = .80` = power_any > .80,
+            `any = .90` = power_any > .90,
+            `any = .95` = power_any > .95
+            ) %>% 
   gather(power, value, -cell_size, convert = TRUE) %>% 
   filter(value) %>% 
   group_by(power) %>% 
@@ -117,49 +154,28 @@ interpolated_power %>%
   
 
 # Visualize the results -------------------------------------------------------------
+# Simulated data
 result_summary %>% 
-  gather(parameter, value, power:p) %>% 
+  gather(parameter, value, power_both:p_any) %>% 
+  separate(parameter, c("parameter", "test")) %>% 
   ggplot() +
   aes(x = cell_size, 
       y = value, 
       color = parameter,
       label = parameter) +
   geom_line(size = 1.1) +
-  geom_point(size = 2) +
-  geom_dl(method = c("last.points")) +
+  geom_dl(method = "smart.grid") +
   guides(color = FALSE) +
   geom_hline(yintercept = c(.05, .80, .90, .95), linetype = "dashed") +
-  scale_y_continuous(limits = c(0, 1), 
+  scale_y_continuous(NULL, limits = c(0, 1), 
                      labels = scales::percent_format(), 
-                     breaks = seq(.1, 1, .1))
-
-
+                     breaks = seq(.1, 1, .1)) +
+  labs(title = "Power as a function of cell size and main effects") +
+  facet_wrap(~test) +
+  theme_bw()
 
 
 
 # Sandbox ---------------------------------------------------------------------------
 
-cell_sizes = 100
 
-# increase the number of groups
-
-df <- 
-  crossing(group = fct_inorder(c("Fixed-Control", 
-                                 "Fixed-Mindfulness",
-                                 "Growth-Control",
-                                 "Growth-Mindfulness")),
-         id = 1:cell_sizes) %>% 
-  mutate(extra_task = c(rtpois(n = cell_sizes, lambda = lambda, b = 15), 
-                        rtpois(n = cell_sizes, lambda = lambda + difference/2, b = 15), 
-                        rtpois(n = cell_sizes, lambda = lambda + difference/2, b = 15), 
-                        rtpois(n = cell_sizes, lambda = lambda + difference, b = 15))) %>% 
-  separate(group, c("mindset", "intervention"))
-  
-
-df %>% 
-  group_by(mindset, intervention) %>% 
-  summarise(mean(extra_task))
-
-glm(extra_task ~ intervention * mindset, family  = "poisson", data = df) %>% 
-  tidy()
-  
