592ba03aa6bbfcc73ef69c2dad185c49d042e24e
nthun <thomassocska@yahoo.com>
2020-08-06 15:55:52 +0200

    Separate preprocessing from data analysis

--- R/data_analysis.R
+++ R/data_analysis.R
@@ -9,177 +9,18 @@ library(broom)
 library(sjPlot)
 library(pscl)
 library(performance)
-library(broom)
 
 theme_set(theme_light())
 
 
-# Define scale recoding -------------------------------------------------------------
-# se_
-agree_4 <- 
-  c("Egyáltalán nem értek egyet" = 0,
-    "Nem értek egyet" = 1,
-    "Egyetértek" = 2,
-    "Teljesen egyetértek" = 3
-  )
+processed_final <- read_csv("data/processed_final.csv")
 
-# ms_, mc_3
-agree_6 <- 
-  c(      "Egyáltalán nem értek egyet" = 1,
-          "Nem értek egyet" = 2,
-          "Inkább nem értek egyet" = 3,
-          "Inkább egyetértek" = 4,
-          "Egyetértek" = 5,
-          "Teljes mértékben egyetértek" = 6
-  )
-
-# cams_
-frequency_4 <-
-  c("Egyáltalán nem / Ritkán" = 1,
-    "Néha" = 2,
-    "Gyakran" = 3,
-    "Majdnem mindig" = 4
-  )
-
-# sc_
-frequency_5 <-
-  c("Szinte soha" = 1,
-    "Ritkán" = 2,
-    "Körülbelül az esetek felében" = 3,
-    "Gyakran" = 4,
-    "Szinte mindig" = 5
-  )
-
-# risc_
-true_4 <- 
-  c("Egyáltalán nem jellemző rám" = 1,
-    "2" = 2,
-    "3" = 3,
-    "Teljes mértékben jellemző rám" = 4
-  )
-
-# grit_
-true_5 <-
-  c("Egyáltalán nem jellemző rám" = 1,
-    "Kevéssé jellemző rám" = 2,
-    "Valamennyire jellemző rám" = 3,
-    "Nagyon jellemző rám" = 4,
-    "Teljes mértékben jellemző rám" = 5
-  )
-
-# outro_experiences_
-true_6 <- 
-  c("Egyáltalán nem" = 1,
-    "2" = 2, 
-    "3" = 3,
-    "4" = 4, 
-    "6" = 5,
-    "Nagyon" = 6
-  )
-
-# previous_iq_score
-iq_5 <- c("Nem emlékszem." = NA,
-          "Az átlagos alatt volt." = 1,
-          "Átlagos volt." = 2,
-          "Átlag fölött volt." = 3,
-          "Sokkal az átlag fölött volt." = 4
-)
-
-
-# Read data ------------------------------------------------------------------
-manual_exclude <- read_lines("data/exclude_by_survey_log.txt")
-
-# Authenticate using a PAT
-osf_pat <- read_lines("osf_pat.txt")
-osf_auth(token = osf_pat)
-
-# Download data into data folder
-osf_retrieve_file("v6kcd") %>%
-  osf_download(path = "data/",
-               conflicts = "overwrite",
-               progress = TRUE)
-
-# Read data
-mfs_raw <- qualtRics::read_survey("data/IQ_MS_MFS_2019_April+6,+2020_13.00.csv")
-
-# Clean the  data
-mfs <- 
-  mfs_raw %>% 
-  clean_names() %>% 
-  # Remove trials
-  filter(status != "Survey Preview") %>% 
-  # Keep only finished experiments
-  filter(finished == TRUE) %>% 
-  # Exclude pilot runs based on research log
-  filter(date(start_date) != "2019-03-18") %>% 
-  filter(date(start_date) != "2019-04-01") %>% 
-  filter(date(start_date) != "2019-04-08") %>% 
-  # Exclude participants based on equipment malfunction (source: lab log), or suspicion (manual coding of open ended suspicion check question)
-  filter(!response_id %in% manual_exclude) %>% 
-  # Exclude those who did not pay attention during the experiment
-  filter(!str_detect(mc_2, "6|7")) %>% 
-  # Recode the manipulated variables for better readability
-  mutate( mindset = case_when(fl_54_do == "Incrementalmindsetmanipulation" ~ "Growth",
-                              fl_54_do == "Fixedmindsetmanipulation" ~ "Fixed"),
-          intervention = case_when(fl_56_do == "Mfsintervention" ~ "Mindfulness",
-                                   fl_56_do == "Controlintervention"  ~ "Control"))
-
-# Recode scale values to a number and keep importantvariables only
-mfs_scales <-
-  mfs %>%
-  # replace ő-s with o-s
-  mutate(across(where(is.character), enc2native)) %>% 
-  transmute(
-    # ids 
-    response_id, start_date, end_date, comput_id,
-    # demographic info
-    gender = recode(gender, !!!gender_cat), 
-    age = year(start_date) - birth_year, 
-    education, term_no, field,
-    # group allocation
-    mindset, intervention, 
-    # recode scales to numeric values
-    across(matches("^ms_\\d+"), ~recode(.x, !!!agree_6)),
-    across(matches("^cams_\\d+"), ~recode(.x, !!!frequency_4)),
-    across(matches("^sc_\\d+"), ~recode(.x, !!!frequency_5)),
-    across(matches("^se_\\d+"), ~recode(.x, !!!agree_4)),
-    across(matches("^risc_\\d+"), ~recode(.x, !!!true_4)),
-    across(matches("^grit_\\d+"), ~recode(.x, !!!true_5)),
-    across(mc_1:mc_2, ~str_extract(.x, "\\d+") %>% as.numeric()),
-    mc_3 = recode(mc_3, !!!agree_6),
-    across(matches("^outro_experiences_\\d+"), ~recode(.x, !!!true_6)),
-    across(matches("^previous_iq_score"), ~recode(.x, !!!iq_5)),
-    across(matches("agt_p_\\d+")),
-    # iq related questions
-    iq_assessed, iq_importance, iq_confid_1,
-    # consumption
-    meditation = mediatation, coffee, alcohol, drug = q325, 
-    # suspicion check questions
-    what1, what2, 
-  )
-
-# Calculate the number of extra tasks and add it to the rest of the variables
-processed_data <-
-  mfs %>% 
-  select(response_id, matches("iq_prac_[^q]*\\d+$")) %>%
-  pivot_longer(names_to = "task", 
-               values_to = "answer", 
-               values_drop_na = TRUE,
-               cols = -response_id) %>% 
-  count(response_id, name = "extra_tasks") %>% 
-  right_join(mfs_scales, by = "response_id") %>% 
-  mutate(extra_tasks = if_else(is.na(extra_tasks), 0L, extra_tasks))
-
-# Processed data ready
-# write_excel_csv(processed_data, "data/processed_data_labels.csv")
-# Create and compare models ---------------------------------------------------------
-processed_data <- read_csv("data/processed_data_labels.csv")
-
-mod_1 <- glm(extra_tasks ~ mindset * intervention, 
+# Data analysis
+mod_1 <- glm(extra_tasks ~ mindset * intervention + gender, 
            family = "poisson", 
            data = processed_data)
 
-mod_2 <- glm(extra_tasks ~ mindset + intervention, 
+mod_2 <- glm(extra_tasks ~ mindset + intervention + gender, 
              family = "poisson",
              data = processed_data)
 
@@ -217,24 +58,25 @@ processed_data %>%
 # 35% of participants completed 0 tasks
 
 mod_3 <- zeroinfl(extra_tasks ~ mindset * intervention, dist = "poisson",
-         data = processed_data) 
+         data = processed_final) 
 
 mod_4 <- zeroinfl(extra_tasks ~ mindset * intervention, dist = "negbin",
-                  data = processed_data)
+                  data = processed_final)
 
 mod_5 <- hurdle(extra_tasks ~ mindset * intervention, 
-                  data = processed_data)
+                dist = "negbin", zero.dist = "negbin",
+                data = processed_data)
 
 summary(mod_5)
 
 doParallel::registerDoParallel()
 
-tab_model(mod_3, mod_4, mod_5,
+tab_model(mod_3, mod_4,
           show.aic = TRUE, show.stat = TRUE, p.style = "numeric", 
           show.loglik = TRUE,
           show.dev = TRUE, 
           show.fstat = TRUE,
-          bootstrap = TRUE,
+          # bootstrap = TRUE,
           iterations = 1000,
           show.zeroinf = TRUE)
 
@@ -246,10 +88,10 @@ summary(mod_5)
 # One sided tests
 
 # Mod 3
-pnorm(2.366, lower.tail = FALSE)
+pnorm(1.13, lower.tail = FALSE)
 
 # Mod 4
-pnorm(1.127, lower.tail = FALSE)
+pnorm(1.520, lower.tail = FALSE)
 
 anova(mod_1, mod_2, test = "LRT")
 
@@ -258,3 +100,16 @@ performance::check_distribution(mod_2) %>% plot()
 performance::(mod_1)
 processed_data %>% 
   summarise(mean(extra_tasks == 0))
+
+
+# Obtain % correct score from iq tests -----------------------------------------
+
+library(lmerTest)
+
+select()
+lmer()
+
+processed_data %>% 
+  ggplot() +
+  aes(x = mindset, fill = intervention, color = intervention, y = iq_warmup) +
+  geom_violin(alpha = .5)
---
+++ R/data_preprocessing.R
@@ -0,0 +1,282 @@
+library(tidyverse)
+library(osfr)
+library(qualtRics)
+library(janitor)
+library(lubridate)
+library(psych)
+
+# Define scale recoding -------------------------------------------------------------
+# se_
+agree_4 <- 
+  c("Egyáltalán nem értek egyet" = 0,
+    "Nem értek egyet" = 1,
+    "Egyetértek" = 2,
+    "Teljesen egyetértek" = 3
+  )
+
+# ms_, mc_3
+agree_6 <- 
+  c(      "Egyáltalán nem értek egyet" = 1,
+          "Nem értek egyet" = 2,
+          "Inkább nem értek egyet" = 3,
+          "Inkább egyetértek" = 4,
+          "Egyetértek" = 5,
+          "Teljes mértékben egyetértek" = 6
+  )
+
+# cams_
+frequency_4 <-
+  c("Egyáltalán nem / Ritkán" = 1,
+    "Néha" = 2,
+    "Gyakran" = 3,
+    "Majdnem mindig" = 4
+  )
+
+# sc_
+frequency_5 <-
+  c("Szinte soha" = 1,
+    "Ritkán" = 2,
+    "Körülbelül az esetek felében" = 3,
+    "Gyakran" = 4,
+    "Szinte mindig" = 5
+  )
+
+# risc_
+true_4 <- 
+  c("Egyáltalán nem jellemző rám" = 1,
+    "2" = 2,
+    "3" = 3,
+    "Teljes mértékben jellemző rám" = 4
+  )
+
+# grit_
+true_5 <-
+  c("Egyáltalán nem jellemző rám" = 1,
+    "Kevéssé jellemző rám" = 2,
+    "Valamennyire jellemző rám" = 3,
+    "Nagyon jellemző rám" = 4,
+    "Teljes mértékben jellemző rám" = 5
+  )
+
+# outro_experiences_
+true_6 <- 
+  c("Egyáltalán nem" = 1,
+    "2" = 2, 
+    "3" = 3,
+    "4" = 4, 
+    "6" = 5,
+    "Nagyon" = 6
+  )
+
+# previous_iq_score
+iq_5 <- c("Nem emlékszem." = NA,
+          "Az átlagos alatt volt." = 1,
+          "Átlagos volt." = 2,
+          "Átlag fölött volt." = 3,
+          "Sokkal az átlag fölött volt." = 4
+)
+
+# gender
+gender_cat <- c("Férfi" = "Male",
+                "Nő" = "Female")
+
+# Read data ------------------------------------------------------------------
+manual_exclude <- read_lines("data/exclude_by_survey_log.txt")
+
+# Authenticate using a PAT
+osf_pat <- read_lines("osf_pat.txt")
+osf_auth(token = osf_pat)
+
+# Download data into data folder
+osf_retrieve_file("v6kcd") %>%
+  osf_download(path = "data/",
+               conflicts = "overwrite",
+               progress = TRUE)
+
+# Read data
+mfs_raw <- qualtRics::read_survey("data/IQ_MS_MFS_2019_April+6,+2020_13.00.csv")
+
+# Clean the  data
+mfs <- 
+  mfs_raw %>% 
+  clean_names() %>% 
+  # Remove trials
+  filter(status != "Survey Preview") %>% 
+  # Keep only finished experiments
+  filter(finished == TRUE) %>% 
+  # Exclude pilot runs based on research log
+  filter(date(start_date) != "2019-03-18") %>% 
+  filter(date(start_date) != "2019-04-01") %>% 
+  filter(date(start_date) != "2019-04-08") %>% 
+  # Exclude participants based on equipment malfunction (source: lab log), or suspicion (manual coding of open ended suspicion check question)
+  filter(!response_id %in% manual_exclude) %>% 
+  # Exclude those who did not pay attention during the experiment
+  filter(!str_detect(mc_2, "6|7")) %>% 
+  # Recode the manipulated variables for better readability
+  mutate( mindset = case_when(fl_54_do == "Incrementalmindsetmanipulation" ~ "Growth",
+                              fl_54_do == "Fixedmindsetmanipulation" ~ "Fixed"),
+          intervention = case_when(fl_56_do == "Mfsintervention" ~ "Mindfulness",
+                                   fl_56_do == "Controlintervention"  ~ "Control"))
+
+# Recode scale values to a number and keep important variables only
+mfs_scales <-
+  mfs %>%
+  # replace ő-s with o-s
+  mutate(across(where(is.character), enc2native)) %>% 
+  transmute(
+    # ids 
+    response_id, start_date, end_date, comput_id,
+    # demographic info
+    gender = recode(gender, !!!gender_cat), 
+    age = year(start_date) - birth_year, 
+    education, term_no, field,
+    # group allocation
+    mindset, intervention, 
+    # recode scales to numeric values
+    across(matches("^ms_\\d+"), ~recode(.x, !!!agree_6)),
+    across(matches("^cams_\\d+"), ~recode(.x, !!!frequency_4)),
+    across(matches("^sc_\\d+"), ~recode(.x, !!!frequency_5)),
+    across(matches("^se_\\d+"), ~recode(.x, !!!agree_4)),
+    across(matches("^risc_\\d+"), ~recode(.x, !!!true_4)),
+    across(matches("^grit_\\d+"), ~recode(.x, !!!true_5)),
+    across(mc_1:mc_2, ~str_extract(.x, "\\d+") %>% as.numeric()),
+    mc_3 = recode(mc_3, !!!agree_6),
+    across(matches("^outro_experiences_\\d+"), ~recode(.x, !!!true_6)),
+    across(matches("^previous_iq_score"), ~recode(.x, !!!iq_5)),
+    across(matches("agt_p_\\d+")),
+    # iq related questions
+    iq_assessed, iq_importance, iq_confid_1,
+    # consumption
+    meditation = mediatation, coffee, alcohol, drug = q325, 
+    # suspicion check questions
+    what1, what2, 
+  )
+
+
+# Calculate IQ performance -----------------------------------------------------
+
+# Calculate the proportion of correct answers for the IQ tasks
+iq_correct_answers <- read_csv("data/iq_correct_answers.csv")
+
+iq_scores <-
+  mfs %>% 
+  select(response_id, iq_correct_answers$task) %>% 
+  pivot_longer(cols = -response_id,
+               names_to = "task",
+               values_to = "answer") %>% 
+  # Unanswered - e.g. out of time - questions will count as bad answers
+  mutate(answer = if_else(is.na(answer), 0, answer)) %>% 
+  left_join(iq_correct_answers, by = "task") %>% 
+  group_by(response_id, block) %>% 
+  summarise(pct_correct = mean(answer == correct_answer), .groups = "drop") %>% 
+  pivot_wider(names_from = "block",
+              values_from = "pct_correct", 
+              names_prefix = "iq_")
+
+# Calculate the number of extra tasks and add it to the rest of the variables
+processed_data <-
+  mfs %>% 
+  select(response_id, matches("iq_prac_[^q]*\\d+$")) %>%
+  pivot_longer(names_to = "task", 
+               values_to = "answer", 
+               values_drop_na = TRUE,
+               cols = -response_id) %>% 
+  count(response_id, name = "extra_tasks") %>% 
+  right_join(mfs_scales, by = "response_id") %>% 
+  mutate(extra_tasks = if_else(is.na(extra_tasks), 0L, extra_tasks)) %>% 
+  left_join(iq_scores, by = "response_id")
+
+# Processed data ready
+# write_excel_csv(processed_data, "data/processed_data_labels.csv")
+
+# Reverse items and calculate scales -------------------------------------------
+
+processed_data <- read_csv("data/processed_data_labels.csv")
+
+# Reverse coding items ---------------------------------------------------------
+
+processed_reversed <-
+  processed_data %>% 
+  # MS scale
+  mutate(across(c(ms_1, ms_3, ms_4), 
+                ~recode(.x, 
+                        `1` = 6,
+                        `2` = 5,
+                        `3` = 4,
+                        `4` = 3,
+                        `5` = 2,
+                        `6` = 1)),
+         # cognitive affective MFS (CAMS) scale
+         across(c(cams_2, cams_6, cams_7), 
+                ~recode(.x, 
+                        `1` = 4,
+                        `2` = 3,
+                        `3` = 2,
+                        `4` = 1)),
+         # self compassion scale
+         across(c(sc_1, sc_4, sc_8, sc_9, sc_11, sc_12),
+                ~recode(.x, 
+                        `1` = 5,
+                        `2` = 4,
+                        `3` = 3,
+                        `4` = 2,
+                        `5` = 1)),
+         # Rosenberg scale
+         across(c(se_3, se_5, se_8, se_9), 
+                ~recode(.x, 
+                        `0` = 3,
+                        `1` = 2,
+                        `2` = 1,
+                        `3` = 0)),
+         # GRIT scale
+         across(c(grit_2, grit_4, grit_7, grit_8), 
+                ~recode(., 
+                        `1` = 5,
+                        `2` = 4,
+                        `3` = 3,
+                        `4` = 2,
+                        `5` = 1
+                )),
+         meditation = fct_relevel(meditation, "Soha")
+  )
+
+# Outro experiences Factror calculate -------------------------------
+
+processed_data %>% 
+  select(starts_with("outro_experiences_")) %>% 
+  GGally::ggpairs()
+
+processed_data %>% 
+  select(starts_with("outro_experiences_")) %>% 
+  nfactors(rotate = "oblimin", fm = "wls")
+
+
+# 3 factor solution is chosen
+fa_3 <-
+  processed_data %>% 
+  select(starts_with("outro_experiences_")) %>% 
+  psych::fa(nfactors = 3, fm = "wls")
+
+fa_scores <- 
+  fa_3$scores %>% 
+  as_tibble() %>% 
+  rename(out_ = everything()) 
+
+
+# Calculating scales by mean ---------------------------------------------------
+
+processed_final <- 
+  processed_reversed %>%
+  mutate(ms_mean = rowMeans(x = select(., starts_with("ms_"))),
+         se_mean = rowMeans(x = select(., starts_with("se_"))),
+         cams_mean = rowMeans(x = select(., starts_with("cams_"))),
+         sc_mean = rowMeans(x = select(., starts_with("sc_"))),
+         risc_mean = rowMeans(x = select(., starts_with("risc_"))),
+         grit_mean = rowMeans(x = select(., starts_with("grit_"))),
+         agp_t_mean = rowMeans(x = select(., starts_with("agt_p_")))
+  ) %>% 
+  # Attach factor scores
+  bind_cols(fa_scores)
+
+write_excel_csv(processed_final, "data/processed_final.csv")
+
